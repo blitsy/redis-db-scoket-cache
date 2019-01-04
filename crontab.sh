e8a7c2c582ba046cd397904fde2530cf9396e8df
Oliver Holmberg <oliver@blitsy.com>
2019-01-04 15:50:46 -0600

    Update crontab.sh

--- crontab.sh
+++ crontab.sh
@@ -1,1 +1,3 @@
-@reboot ~/redis-db-scoket-cahce/socket-server.py >> ~/redis-db-scoket-cahce/logs/server-log.out 2>&1
+@reboot python ~/redis-db-scoket-cache/socket-server.py >> ~/server_out.log 2>&1
+
+* * * * * sh /home/pi/cron.sh >> /home/pi/data_cache_log.out 2>&1
